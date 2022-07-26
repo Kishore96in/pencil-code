8b14ab0c1316556cfbf4725d362267dc80fa4e3e
Kishore Gopalakrishnan <kishore96@gmail.com>
2022-07-26 11:10:51 +0530

    Update example

--- samples/continuous-forcing-from-file/generate_forcing_cont.py
+++ samples/continuous-forcing-from-file/generate_forcing_cont.py
@@ -7,11 +7,11 @@ import numpy as np
 
 dim = pc.read.dim()
 
-a = np.zeros((3,dim.nx,dim.ny,dim.nz))
+a = np.zeros((3,dim.nz,dim.ny,dim.nx))
 
 a[1,:,:,:] = 0.1
 a[2,:,:,:] = 0.2
 a[0,1,1,1] = 0.05
-a[0,2,1,0] = 0.01
+a[0,0,1,2] = 0.01
 
 pc.util.write_forcing_cont(a)
