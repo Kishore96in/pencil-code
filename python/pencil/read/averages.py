Kishore96in/pencil-code

commit 26d00d4873a917aa1382ba5a09917208060d039a

Python: Averages.read: warning when both 1D and 2D averages are read.
If the times of the 1D and 2D averages differ, the returned averages
object may get assigned any one of them as the 't' attribute (with the
times of the other kind of averages not being returned at all). I have
changed the code to save the time as an attribute to each plane.

python/pencil/read/averages.py
@@ -78,7 +78,17 @@ class Averages(object):
 
         import numpy as np
 
-        self.t = np.array([])
+        self._t = np.array([])
+
+    @property
+    def t(self):
+        return self._t
+
+    @t.setter
+    def set_t(self, arr):
+        if len(self.t) > 0 and np.any(self.t != arr):
+            warnings.warn("Mismatch between the times of different kinds of averages (usually happens when 1D and 2D averages are stored at different times). Please use the t attributes of the respective planes (e.g. av.xy.t, rather than av.t).")
+        self._t = arr
 
     def keys(self):
         for i in self.__dict__.keys():
@@ -344,6 +354,7 @@ class Averages(object):
                     plane_keys.remove("keys")
                 setattr(ext_object, "keys", plane_keys)
 
+                ext_object.t = t
                 self.t = t
                 setattr(self, plane, ext_object)
 
@@ -421,6 +432,7 @@ class Averages(object):
                         comp_time=comp_time,
                     )
 
+                ext_object.t = t
                 self.t = t
                 setattr(self, plane, ext_object)
         return 0

commit a083deca44acf0f8986ac2d66b551b3b9750e40d

Python: Averages: fix bug in checking t
Earlier code was not correctly handling arrays of different lengths.

Bug introduced in commit 26d00d4873a917aa1382ba5a09917208060d039a

python/pencil/read/averages.py
@@ -85,7 +85,7 @@ class Averages(object):
 
     @t.setter
     def t(self, arr):
-        if len(self.t) > 0 and np.any(self.t != arr):
+        if len(self.t) > 0 and (len(self.t) != len(arr) or np.any(self.t != arr)):
             warnings.warn("Mismatch between the times of different kinds of averages (usually happens when 1D and 2D averages are stored at different times). Please use the t attributes of the respective planes (e.g. av.xy.t, rather than av.t).")
         self._t = arr
 