b225a486c43ee519365896d69ee02c97588a22b5
Kishore Gopalakrishnan <kishore96@gmail.com>
2025-04-22 18:14:07 +0530

    Python: sim.compile: fix conflict between raise_errors and autoclean

--- python/pencil/sim/simulation.py
+++ python/pencil/sim/simulation.py
@@ -668,6 +668,7 @@ class __Simulation__(object):
         verbose=False,
         hostfile=None,
         autoclean=True,
+        raise_errors=False,
         **kwargs,
         ):
         """Compiles the simulation. Per default the linking is done before the
@@ -712,6 +713,7 @@ class __Simulation__(object):
             command=" ".join(command),
             verbose=verbose,
             logfile=join(self.pc_dir, "compilelog_" + timestamp),
+            raise_errors=(raise_errors and not autoclean), #if auto-clean is specified, we should ignore errors here
             **kwargs,
             )
 
@@ -723,6 +725,7 @@ class __Simulation__(object):
                 fast=fast,
                 verbose=verbose,
                 hostfile=hostfile,
+                raise_errors=raise_errors,
                 **kwargs,
                 )
         else:
