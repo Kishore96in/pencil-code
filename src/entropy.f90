378ac94ddc353e56616e3531e763834beed610a4
Kishore Gopalakrishnan <kishore96@gmail.com>
2025-05-27 14:38:15 +0530

    entropy: refactor pencil requests for cooling
    Add p%TT for cooltype='corona', which was not earlier requested.

--- src/entropy.f90
+++ src/entropy.f90
@@ -2930,27 +2930,32 @@ module Energy
         lpenc_requested(i_rho1)=.true.
       endif
 !
-      if (cool/=0.0 .or. cool_ext/=0.0 .or. cool_int/=0.0) then
-        if (cooltype=='rho_cs2') then
-          lpenc_requested(i_cs2)=.true.
+      if (cool/=0.0 .or. cool_ext/=0.0 .or. cool_int/=0.0 .or. luminosity/=0.0) then
+        if (cooltype=='Lambda-constant') then
           lpenc_requested(i_rho)=.true.
-        endif
-        if (cooltype=='pressure'.or.cooling_profile=='surface_pp') then
-          lpenc_requested(i_cs2)=.true.
-          lpenc_requested(i_pp)=.true.
-        endif
-        if (cooltype=='two-layer'.or.cooltype=='square-well') then
-          lpenc_requested(i_cs2)=.true.
-          lpenc_requested(i_rho)=.true.
-        endif
-        if (cooltype=='corona') then
+        elseif (cooltype=='corona') then
           lpenc_requested(i_TT)=.true.
           lpenc_requested(i_cv)=.true.
           lpenc_requested(i_rho)=.true.
+        elseif (cooltype=='rho_cs2' &
+          .or. cooltype=='two-layer' &
+          .or. cooltype=='square-well' &
+          .or. cooltype=='two-layer-mean') then
+          lpenc_requested(i_cs2)=.true.
+          lpenc_requested(i_rho)=.true.
+        elseif (cooltype=='cs2-rho'.or.cooltype=='Temp-rho') then
+          lpenc_requested(i_cs2)=.true.
+          lpenc_requested(i_rho1)=.true.
+        elseif (cooltype=='entropy') then
+          lpenc_requested(i_ss)=.true.
+        elseif (cooltype=='pressure'.or.cooling_profile=='surface_pp') then
+          lpenc_requested(i_cs2)=.true.
+          lpenc_requested(i_pp)=.true.
+        else
+          lpenc_requested(i_cs2)=.true.
         endif
       endif
 !
-      if (lgravz .and. (luminosity/=0.0 .or. cool/=0.0)) lpenc_requested(i_cs2)=.true.
       if (luminosity/=0 .or. cool/=0 .or. tau_cor/=0 .or. &
           tauheat_buffer/=0 .or. heat_uniform/=0 .or. &
           cool_uniform/=0 .or. cool_newton/=0 .or. &
@@ -2960,6 +2965,7 @@ module Energy
         lpenc_requested(i_rho1)=.true.
         lpenc_requested(i_TT1)=.true.
       endif
+!
       if (pretend_lnTT) then
         lpenc_requested(i_uglnTT)=.true.
         lpenc_requested(i_divu)=.true.
