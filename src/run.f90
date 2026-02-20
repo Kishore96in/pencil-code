eb8a5bc5fa9c3196a2769549a41cf815764e25d4
Kishore G. <kishore96.work@gmail.com>
2026-02-20 17:26:28 +0530

    run.f90: call check_curla_consistenct as early as possible

--- src/run.f90
+++ src/run.f90
@@ -1057,6 +1057,7 @@ endsubroutine helper_loop
 !  time step even if we didn't read ghost zones.
 !
   call update_ghosts(f)
+  call check_curla_consistency(f, caller='run.f90 before timeloop')
 !
 !  Allow here for the possibility to have spectral output
 !  from the first time. This works for all variables, except
